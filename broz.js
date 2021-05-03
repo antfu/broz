da669d6f421f3b3267b5efd4ffd107485fa099f5
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-05-03 15:48:52 +0200

    style: fix lint

--- broz.js
+++ broz.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
-const { execFileSync } = require("child_process");
+const { execFileSync } = require('child_process')
 const path = require('path')
 
 const args = [path.resolve(__dirname, 'index.js'), process.argv[2] || '']
-execFileSync(String(require("electron")),args)
+execFileSync(String(require('electron')), args)

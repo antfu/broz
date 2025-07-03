5e4e1d5fdaff9693fd9d7bba84182f04970a9175
Anthony Fu <github@antfu.me>
2025-07-03 14:27:56 +0900

    chore: update

--- broz.js
+++ broz.js
@@ -4,5 +4,8 @@ const { execFileSync } = require('node:child_process')
 const path = require('node:path')
 const process = require('node:process')
 
-const args = [path.resolve(__dirname, 'index.js'), ...Array.from(process.argv).slice(2)]
+const args = [
+  path.resolve(__dirname, 'index.js'),
+  ...Array.from(process.argv).slice(2),
+]
 execFileSync(String(require('electron')), args, { stdio: 'inherit' })
